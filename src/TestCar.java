1d2073b2dfd4b26f5e5d67b816e56a06d98c6db9
aryanarvekar <narvekar003@gmail.com>
2017-05-09 13:22:45 -0700

    second commit
    Signed-off-by: aryanarvekar <narvekar003@gmail.com>

--- src/TestCar.java
+++ src/TestCar.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 public class TestCar {
 
 	@Test
-	public void test() {
+	public void testForfail() {
 		Car c = new Car();
 		c.setRate(50);
 		assertEquals(50,c.getengineCapacity());
 	}
 
+	@Test
+	public void testforsuccess() {
+		Car c = new Car();
+		c.setRate(150);
+		assertEquals(150,c.getengineCapacity());
+	}
 }
